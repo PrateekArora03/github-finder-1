1630a955db89ba58604dd6160cf9108cd8ba44c5
shashank <shashankbhatotia@gmail.com>
2019-08-27 17:45:55 +0530

    Add componentDidMount lifecycle method to App component and update other components accordingly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Fragment, Component } from 'react';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    console.log("cdm-app");
+
+    this.setState({ loading: true });
+    const res= await axios.get('https://api.github.com/users');
+    console.log("data ", res.data);
+    this.setState({
+      users: res.data,
+      loading: false
+    })
+  };
 
   render () {
-      return (
+    console.log("rndr-app");
+
+    const { users, loading } = this.state;
+
+    return (
       <Fragment>
         <div className="App">
           <Navbar />
           <div className="container">
-            <Users />
+            <Users loading={loading} users={users} />
           </div>
         </div>
       </Fragment>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,41 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './Useritem';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: "2",
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt"
-      },
-      {
-        id: "3",
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett "
-      }
-    ]
-  }
+const Users = (props) => {
+  const { users, loading } = props;
 
-  render() {
-    return (
-      <div style={userStyle}>
-        {
-          this.state.users.map(user => (
-            <UserItem key={user.id} user={user} />
+  return (
+    <div style={userStyle}>
+      {
+        loading ?
+          <h4 className="text-center">loading...</h4> :
+          users.map(user => (
+            <UserItem key={user.id} user={user}/>
           ))
-        }
-      </div>
-    )
-  }
+      }
+    </div>
+  )
 }
 
 const userStyle = {
