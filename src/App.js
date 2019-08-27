c31488de3f63abdd9aab62124ef7edb863d8169a
shashank <shashankbhatotia@gmail.com>
2019-08-27 15:55:30 +0530

    Add props and propTypes in Navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
 
   render () {
-      const name = 'John Doe';
-      const loading = false;
-      const showName = false;
 
       return (
       <Fragment>
         <div className="App">
-          { loading ? <h4>Loading...</h4> : <h1>Hello { showName && name }</h1> }
+          <Navbar />
         </div>
       </Fragment>
     );
