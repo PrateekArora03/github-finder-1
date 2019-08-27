0587f50d21828833c0395e6d3fb94d6f12b593f8
shashank <shashankbhatotia@gmail.com>
2019-08-27 15:33:19 +0530

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  render () {
+      const name = 'John Doe';
+      const loading = false;
+      const showName = false;
+
+      return (
+      <Fragment>
+        <div className="App">
+          { loading ? <h4>Loading...</h4> : <h1>Hello { showName && name }</h1> }
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
