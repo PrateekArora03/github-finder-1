9a0248b0afc24fdbb01a02f46faadee718eff15b
shashank <shashankbhatotia@gmail.com>
2019-08-28 13:34:32 +0530

    Create About component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -59,16 +60,22 @@ class App extends Component {
     const { users, loading } = this.state;
 
     return (
-      <Fragment>
+      <Router>
         <div className="App">
           <Navbar />
           <div className="container">
             <Alert alert={this.state.alert} />
-            <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length > 0 ? true : false } setAlert={this.setAlert} />
-            <Users loading={loading} users={users} />
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length > 0 ? true : false } setAlert={this.setAlert} />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )} />
+            </Switch>
           </div>
         </div>
-      </Fragment>
+      </Router>
     );
   }
 }
