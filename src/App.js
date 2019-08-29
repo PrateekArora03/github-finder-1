0fd636657e6fb66fe996c5a4669990c2a6136f13
shashank <shashankbhatotia@gmail.com>
2019-08-29 13:19:43 +0530

    Create Reducer Actions

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ const App = () => {
   const [alert, setAlertState] = useState(null);
 
   // search Github users
-  const searchUsers = async text => {
-    setLoading(true)
-    
-    const res= await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
+  
 
   // get single github user
   const getUser = async (username) => {
@@ -71,7 +64,7 @@ const App = () => {
             <Switch>
               <Route exact path="/" render={props => (
                 <Fragment>
-                  <Search searchUsers={searchUsers} clearUsers={clearUsers} showClear={ users.length > 0 ? true : false } setAlert={setAlert} />
+                  <Search clearUsers={clearUsers} showClear={ users.length > 0 ? true : false } setAlert={setAlert} />
                   <Users loading={loading} users={users} />
                 </Fragment>
               )} />
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext'
 
-const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext);
+
   const [text, setText] = useState('');
 
   const onChange = ({ target: { value }}) => {
@@ -13,8 +16,7 @@ const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      // passing the props upward
-      searchUsers(text);
+      githubContext.searchUsers(text);
       // resetting the state
       setText('');
     };
@@ -39,7 +41,6 @@ const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
 }
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
